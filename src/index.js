b91008dc600e4105a2437019b5b2617047363733
1we7 <xlwey.pro@gmail.com>
2026-05-26 21:08:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, Partials, REST, Routes } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const db = require('./database');
@@ -23,6 +23,8 @@ client.invitesCache = new Map();
 // Handler de commandes récursif pour lire les sous-dossiers
 const commandsPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(commandsPath);
+const commandsData = [];
+
 for (const folder of commandFolders) {
     const folderPath = path.join(commandsPath, folder);
     const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
@@ -30,6 +32,7 @@ for (const folder of commandFolders) {
         const command = require(path.join(folderPath, file));
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
+            commandsData.push(command.data.toJSON());
         }
     }
 }
@@ -46,5 +49,24 @@ for (const file of eventFiles) {
         client.on(event.name, (...args) => event.execute(...args, client));
     }
 }
+
+// Auto-deploy des commandes au démarrage
+async function deployCommands() {
+    try {
+        const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+        console.log(`Déploiement de ${commandsData.length} commandes...`);
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commandsData }
+        );
+        console.log('Commandes déployées avec succès.');
+    } catch (error) {
+        console.error('Erreur lors du déploiement des commandes :', error);
+    }
+}
+
+client.once('ready', async () => {
+    await deployCommands();
+});
 
 client.login(process.env.TOKEN);
