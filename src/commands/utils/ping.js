7540c79c251ced4c006a75b0bc39d2b19bb0016e
1we7 <xlwey.pro@gmail.com>
2026-05-27 13:32:55 +0200

    Update ping.js

--- src/commands/utils/ping.js
+++ src/commands/utils/ping.js
@@ -3,11 +3,11 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Latence du bot'),
-
-    async execute(interaction) {
-        const sent = await interaction.reply({ content: '🏓 Calcul...', fetchReply: true });
+        .setDescription('Répond avec la latence du bot !'),
+    async execute(interaction, db) {
+        const sent = await interaction.reply({ content: 'Calcul du ping...', fetchReply: true });
         const latency = sent.createdTimestamp - interaction.createdTimestamp;
-        interaction.editReply(`🏓 Pong ! Latence : **${latency}ms** | API : **${interaction.client.ws.ping}ms**`);
-    }
+        
+        await interaction.editReply(`🏓 Pong ! Latence API : **${latency}ms**`);
+    },
 };
